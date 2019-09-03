d818fbd8a79339c778d7c910b22041a199c16e53
xsu <xsu@thoughtworks.com>
2019-09-03 23:46:39 +0800

    more complex.

--- src/main/java/com/tw/apistackbase/controller/HelloController.java
+++ src/main/java/com/tw/apistackbase/controller/HelloController.java
@@ -4,12 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    public Map<String, String> hello() {
+        Map<String, String> result = new HashMap<>();
+        result.put("msg1", "hello");
+        result.put("msg2", "world");
+        return result;
     }
 
 }
